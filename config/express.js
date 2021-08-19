f30ddf0b740bd0754e0d2d174ee8934374a46409
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-08-19 23:59:00 +0200

    add express-handlebars

--- config/express.js
+++ config/express.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 
 module.exports = (app) => {
+    app.engine('hbs', hbs({
+        extname: '.hbs'
+    }));
+    
+    app.set('view engine', 'hbs');
+    
     app.use('/', express.static('static'));
     app.use(express.urlencoded({ extended: true })); // body-parser as middleware
     app.use(cookieParser());
